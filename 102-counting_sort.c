c4592a492ef1ca8dbfa7f0858e79985e5fa0f995
wessam18 <wessamabdelnabi903@gmail.com>
2023-12-18 23:39:04 +0200

    "betty"

--- 102-counting_sort.c
+++ 102-counting_sort.c
@@ -3,19 +3,19 @@
  * max - function that get the maximum number of an array
  * @array: the input to get the max from it
  * @size: the length of the array
- * Return: get the(max) number  
+ * Return: get the(max) number
 */
 int max(int *array, size_t size)
 {
 size_t x;
 int max_num = array[0];
 
-    for (x = 1; x < size; x++)
-    {
-        if (array[x] > max_num)
-            max_num = array[x];
-    }
-    return (max_num);
+	for (x = 1; x < size; x++)
+	{
+		if (array[x] > max_num)
+			max_num = array[x];
+	}
+	return (max_num);
 }
 
 
@@ -31,36 +31,36 @@ int *count, *sorted_arr;
 int max_num = max(array, size);
 size_t k = max_num + 1;
 
-    if (!array || size < 2)
-        return;
+	if (!array || size < 2)
+		return;
 
-    count = malloc(k * sizeof(size_t));
-    if (count == NULL)
-        return;
+	count = malloc(k * sizeof(size_t));
+	if (count == NULL)
+		return;
 
-    sorted_arr = malloc(size * sizeof(size_t));
-    if (sorted_arr == NULL)
-    {
-        free(count);
-        return;
-    }
+	sorted_arr = malloc(size * sizeof(size_t));
+	if (sorted_arr == NULL)
+	{
+		free(count);
+		return;
+	}
 
-    for (y = 0; y < size; y++)
-        count[array[y]]++;
+	for (y = 0; y < size; y++)
+		count[array[y]]++;
 
-    for (y = 0; y < k; y++)
-        count[y] += count[y - 1];
+	for (y = 0; y < k; y++)
+		count[y] += count[y - 1];
 
-    print_array(count, k);
+	print_array(count, k);
 
-    for (y = (size - 1); y > 0 ; --y)
-        sorted_arr[--count[array[y]]] = array[y];
+	for (y = (size - 1); y > 0 ; --y)
+		sorted_arr[--count[array[y]]] = array[y];
 
-    for (y = 0; y < size; y++)
-        array[y] = sorted_arr[y];
+	for (y = 0; y < size; y++)
+		array[y] = sorted_arr[y];
 
-    free(sorted_arr);
-    free(count);
+	free(sorted_arr);
+	free(count);
 
 
 }
